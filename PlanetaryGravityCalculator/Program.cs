4c7c836eca8a6d28051ffa8953b17c0a3a3c6c20
Joe Reid <jreid@vnet.net>
2014-06-15 15:11:09 -0500

    broken regex splits

--- PlanetaryGravityCalculator/Program.cs
+++ PlanetaryGravityCalculator/Program.cs
@@ -10,19 +10,19 @@ namespace PlanetaryGravityCalculator
         {
             FileStream inFile = new FileStream(args[0], FileMode.Open);
             StreamReader sReader = new StreamReader(inFile);
-            string line;
             int mass = Convert.ToInt32(sReader.ReadLine());
             int numPlanets = Convert.ToInt32(sReader.ReadLine());
             for (int i = 0; i < numPlanets; i++)
             {
                 string planetLine = sReader.ReadLine();
                 string[] splits = planetLine.Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries);
-                //Regex reg = new Regex(@"^.*, \d+, \d+$");
-                //MatchCollection matches = reg.Matches(planetLine);
+                //Regex reg = new Regex(@"^(.*), (\d+), (\d+)$");
+                //string[] matches = reg.Split(planetLine);
+                //continue;
                 string planetName = splits[0];
                 int planetRadius = Convert.ToInt32(splits[1]);
                 int planetDesity = Convert.ToInt32(splits[2]);
-                Planet planet = new Planet(planetRadius, planetDesity);
+                Planet planet = new Planet(planetName, planetRadius, planetDesity);
                 Console.WriteLine("{0}: {1:0.000}", planetName, planet.gravitationalForce(mass));
                 if (sReader.EndOfStream)
                 {
