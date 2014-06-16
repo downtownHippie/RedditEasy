a835529e89424e7b9246fae0d7ca6ea55017d476
Joe Reid <jreid@vnet.net>
2014-06-16 14:55:28 -0500

    updataed with regex and extra tests

--- PlanetaryGravityCalculator/Program.cs
+++ PlanetaryGravityCalculator/Program.cs
@@ -15,15 +15,23 @@ namespace PlanetaryGravityCalculator
             for (int i = 0; i < numPlanets; i++)
             {
                 string planetLine = sReader.ReadLine();
-                string[] splits = planetLine.Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries);
-                //Regex reg = new Regex(@"^(.*), (\d+), (\d+)$");
-                //string[] matches = reg.Split(planetLine);
-                //continue;
-                string planetName = splits[0];
-                int planetRadius = Convert.ToInt32(splits[1]);
-                int planetDesity = Convert.ToInt32(splits[2]);
+                //string[] splits = planetLine.Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries);
+                Regex reg = new Regex(@"^(.*),\s(\d+),\s(\d+)$");
+                string[] matches = reg.Split(planetLine);
+                // regex returns an empty string if a match exists and begin and end of line, so...
+                if (matches.Length != 5)
+                {
+                    Console.WriteLine("Not enough elements: {0}", planetLine);
+                    continue;
+                }
+                //string planetName = splits[0];
+                string planetName = matches[1];
+                //int planetRadius = Convert.ToInt32(splits[1]);
+                int planetRadius = Convert.ToInt32(matches[2]);
+                //int planetDesity = Convert.ToInt32(splits[2]);
+                int planetDesity = Convert.ToInt32(matches[3]);
                 Planet planet = new Planet(planetName, planetRadius, planetDesity);
-                Console.WriteLine("{0}: {1:0.000}", planetName, planet.gravitationalForce(mass));
+                Console.WriteLine("{0}: {1:0.000}", planet.Name, planet.gravitationalForce(mass));
                 if (sReader.EndOfStream)
                 {
                     Console.WriteLine("Reached end of stream before {0} planets read.", numPlanets);
